10870be5cf95ad80f0a321332237314c80f6b3cd
sirnwksup1 <1605532208@qq.com>
2022-06-30 18:23:12 +0800

    add .players command(QQ)
    Known Issues: the responce may be incomplete

--- src/com/kazuha/mireport/botcontatmodule/RecievePlayerReport.java
+++ src/com/kazuha/mireport/botcontatmodule/RecievePlayerReport.java
@@ -1,12 +1,20 @@
 package com.kazuha.mireport.botcontatmodule;
 
 
+import com.google.common.collect.Lists;
 import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.bungee.event.MiraiFriendMessageEvent;
 import me.dreamvoid.miraimc.bungee.event.MiraiGroupMessageEvent;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import static com.kazuha.mireport.main.*;
 import static com.kazuha.mireport.utils.publicutisl.*;
@@ -16,15 +24,33 @@ public class RecievePlayerReport implements Listener {
     public void RecieveGroup(MiraiGroupMessageEvent e){
         if(e.getGroupID()!=config.getLong("admin-group-num"))return;
         if(e.getMessage().equalsIgnoreCase(".help")){
-            e.sendMessage("§f§lQQ内可用命令列表");
-            e.sendMessage("§2.举报 <目标> <理由> §f 举报一个玩家");
             if(MiraiBot.getBot(config.getLong("botaccount")).getGroup(config.getLong("admin-group-num")).getMember(e.getSenderID()) != null){
-                e.sendMessage("§2.ban <目标> §f 封禁一个玩家");
-                e.sendMessage("§2.unban <目标> §f 解封一个玩家");
-                e.sendMessage("§2.mute <目标> §f 禁言一个玩家");
-                e.sendMessage("§2.kick <目标> §f 踢出一个玩家");
-                e.sendMessage("§2.cmd <命令> §f (BC)执行一个命令");
+                e.sendMessage("QQ内可用命令列表\n.举报 <目标> <理由>举报一个玩家\n.players 获取在线玩家列表 \n.ban <目标> 封禁一个玩家\n.unban <目标>  解封一个玩家\n.mute <目标>  禁言一个玩家\n.kick <目标>  踢出一个玩家\n.cmd <命令>  (BC)执行一个命令");
+
+            }else{
+                e.sendMessage("QQ内可用命令列表\n.举报 <目标> <理由>举报一个玩家");
             }
+        }
+        if(e.getMessage().startsWith(".players")){
+            if(e.getGroupID()!=config.getLong("admin-group-num"))return;
+            if(MiraiBot.getBot(config.getLong("botaccount")).getGroup(config.getLong("admin-group-num")).getMember(e.getSenderID()) == null){
+                e.sendMessage("你没有权限");
+                return;
+            }
+            int player_count = ProxyServer.getInstance().getOnlineCount();
+            StringBuffer output = new StringBuffer();
+            output = output.append( "服务器在线玩家("+ player_count+ "):");
+            Iterator<String> iterator = ProxyServer.getInstance().getServers().keySet().iterator();
+            while (iterator.hasNext()){
+                StringBuffer nor = new StringBuffer();
+                Collection<ProxiedPlayer> player = ProxyServer.getInstance().getServerInfo(iterator.next()).getPlayers();
+                List<ProxiedPlayer> players = Lists.newArrayList(player);
+                for(ProxiedPlayer p : players){
+                    nor.append(p.getName()).append(", ");
+                }
+                output.append("\n[").append(ProxyServer.getInstance().getServerInfo(iterator.next()).getName()).append("]").append(nor);
+            }
+            e.sendMessage(output.toString());
         }
         if(e.getMessage().startsWith(".cmd ")){
             if(MiraiBot.getBot(config.getLong("botaccount")).getGroup(config.getLong("admin-group-num")).getMember(e.getSenderID()) == null){
@@ -83,15 +109,7 @@ public class RecievePlayerReport implements Listener {
     @EventHandler
     public void RecievePlayerReportFromQQ(MiraiFriendMessageEvent e){
         if(e.getMessage().equalsIgnoreCase(".help")){
-            e.sendMessage("§f§lQQ内可用命令列表");
-            e.sendMessage("§2.举报 <目标> <理由> §f 举报一个玩家");
-            if(MiraiBot.getBot(config.getLong("botaccount")).getGroup(config.getLong("admin-group-num")).getMember(e.getSenderID()) != null){
-                e.sendMessage("§2.ban <目标> §f 封禁一个玩家");
-                e.sendMessage("§2.unban <目标> §f 解封一个玩家");
-                e.sendMessage("§2.mute <目标> §f 禁言一个玩家");
-                e.sendMessage("§2.kick <目标> §f 踢出一个玩家");
-                e.sendMessage("§2.cmd <命令> §f (BC)执行一个命令");
-            }
+                e.sendMessage("QQ内可用命令列表\n.举报 <目标> <理由>举报一个玩家");
         }
         if(e.getMessage().startsWith(".unban ")){
             if(MiraiBot.getBot(config.getLong("botaccount")).getGroup(config.getLong("admin-group-num")).getMember(e.getSenderID()) == null){
